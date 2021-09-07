3b0ef7977fab5c82c0567625e601635da79f83cf
Pjotrs OSnovskis <pjotrs.osnovskis@gmail.com>
2021-09-07 21:03:08 +0100

    Set up flass message for contact form as an unordered list item

--- run.py
+++ run.py
@@ -1,19 +1,23 @@
 import os
 
 import json
-#Import Flask class
-from flask import Flask, render_template
+# Import Flask class
+from flask import Flask, render_template, request, flash
 # Creating an instance of it and storing in a variable
+
+if os.path.exists("env.py"):
+    import env
+
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
-
-# Index route decorator
+# Index route 
 @app.route("/")
 def index():
     return render_template("index.html")
 
 
-# About route decorator
+# About route 
 @app.route("/about")
 def about():
     data = []
@@ -32,13 +36,16 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
 
-# Contact route decorator
-@app.route("/contact")
+# Contact route 
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
-# Careers route decorator
+# Careers route 
 @app.route("/careers")
 def careers():
     return render_template("careers.html", page_title="Careers")
